0e184bee0ae1eb04747ba29eb7c838fe4c84a7b5
stigok <stig@stigok.com>
2015-12-13 09:54:44 +0100

    Code cleanup. Updated dependencies

--- app.js
+++ app.js
@@ -1,46 +1,40 @@
 const express = require('express');
-const logger = require('morgan');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const http = require('http');
 
 const app = express();
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(session({secret: 'CHANGE_THIS_SECRET', resave: false, saveUninitialized: true}));
+app.use(session({
+  secret: 'CHANGE_THIS_SECRET',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/', require('./routes/oauth'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: err
   });
 });
 
-module.exports = app;
+http.createServer(app).listen(3000, function (err) {
+  if (err) {
+    throw err;
+  }
+
+  console.log('Listening on http://localhost:3000');
+});
